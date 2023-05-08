d632459a89a128233e3796d5c63695b749c89df7
Documenter.jl <documenter@juliadocs.github.io>
2023-05-08 07:02:26 +0000

    build based on 2a6a890

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CWBProjectSummaryDatasets","category":"page"},{"location":"#CWBProjectSummaryDatasets","page":"Home","title":"CWBProjectSummaryDatasets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CWBProjectSummaryDatasets.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CWBProjectSummaryDatasets]","category":"page"}]
+}
