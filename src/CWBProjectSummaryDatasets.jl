2a6a8909147c834f43174df71181deccf7719d89
okatsn <okatsn@gmail.com>
2023-05-08 14:59:18 +0800

    Initiate the project

--- src/CWBProjectSummaryDatasets.jl
+++ src/CWBProjectSummaryDatasets.jl
@@ -1,5 +1,14 @@
 module CWBProjectSummaryDatasets
 
-# Write your package code here.
 
+    using SmallDatasetMaker
+   # (required) See also `SmallDatasetMaker.datasets`.
+
+    function CWBProjectSummaryDatasets.dataset(package_name, dataset_name)
+        SmallDatasetMaker.dataset(CWBProjectSummaryDatasets,package_name, dataset_name)
+    end 
+    # (optional but recommended) 
+    # To allow direct use of `dataset` without `SmallDatasetMaker`.
+
+    CWBProjectSummaryDatasets.datasets() = SmallDatasetMaker.datasets(CWBProjectSummaryDatasets) 
 end
