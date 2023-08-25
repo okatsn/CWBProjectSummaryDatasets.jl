6c048f1ef67c15ececb95d68049847c55c44fee1
okatsn <okatsn@gmail.com>
2023-08-25 14:00:25 +0800

    Add station table and event catalog.

--- changelog.md
+++ changelog.md
@@ -2,3 +2,6 @@
 
 ## v0.1.0
 - Initiating the project.
+
+## v0.4.0
+- Add station table and event catalog.
---
+++ docs/scripts/station_catalog.jl
@@ -0,0 +1,18 @@
+using DataFrames, CSV
+using Chain
+using Test
+using CWBProjectSummaryDatasets
+using OkFiles
+using SmallDatasetMaker
+
+# Unified file name to avoid too many duplicated compressed at one commit.
+file_catalog = "EventMag5/Catalog.csv"
+file_station = "GeoEMStation/StationInfo.csv"
+
+# # Load the data to see if they are fine
+# catalog = CSV.read(file_catalog, DataFrame)
+# station_location = CSV.read(file_station, DataFrame)
+
+for fpath in [file_catalog, file_station]
+    compress_save(CWBProjectSummaryDatasets, fpath)
+end
